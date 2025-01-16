e2835fd86ca057bc2f552cb0feccf12e6731a98a
Danieltanjw <danieltanjiawei@gmail.com>
2025-01-16 22:53:09 +0800

    Modify deploy.yml

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://Danieltanjw.github.io',
+  url: 'https://danieltanjw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/danieltjw-pf',
